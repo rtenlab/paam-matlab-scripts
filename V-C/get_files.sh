05996c60471d964ba422811c4b05d8a8e576203b
Daniel Enright <Denri006@ucr.edu>
2024-03-04 11:19:23 -0800

    Update get_files.sh

--- V-C/get_files.sh
+++ V-C/get_files.sh
@@ -1,3 +1,4 @@
 #! /bin/bash 
 
 scp  -P 25564 -r paam@169.235.21.73:~/Research/data/V-C/ ./
+scp -P 25564 -r paam@169.235.21.73:/home/paam/Research/cuda-samples/Samples/3_CUDA_Features/StreamPriorities/*.csv ./
